2e2adfdd432e86230fefd390507274f375bfc996
IamSunday <1123448247@qq.com>
2024-11-19 02:51:25 +0800

    first commit

---
+++ mytrain.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+if __name__ == '__main__':
+    # 加载模型
+    # model = YOLO("yolov8-CBAM.yaml")  # 从头开始构建新模型
+    model = YOLO("yolov8n.pt")  # 从头开始构建新模型
+    model.load('yolov8n.pt')  # 加载预训练模型（推荐用于训练）
+    # Use the model
+    results = model.train(data='F:/deep-learning-model/yolov8/ultralytics/data/qiaoliang.yaml',epochs=200,amp=True,name='yolov8-auto-n')  # 训练模型
